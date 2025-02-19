af813fe27aaa07a0faf399f5860e8f710455487c
RyuJirayu123 <rew254967@gmail.com>
2025-02-19 15:11:10 +0700

    Chapter07

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require('express')
+
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 //Load env vars
 
 dotenv.config({path:'./config/config.env'});
-
+connectDB();
+const hospitals = require('./routes/hospitals');
 const app=express();
 
-const hospitals = require('./routes/hospitals');
+app.use(express.json());
 app.use('/api/v1/hospitals', hospitals);
 
 const PORT=process.env.PORT || 5000;
-app.listen (PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ',PORT));
+const server = app.listen (PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ',PORT));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
