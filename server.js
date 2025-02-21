de52911c28f29fd6aede8349fb9b71c87d77edea
RyuJirayu123 <rew254967@gmail.com>
2025-02-21 09:57:09 +0700

    add Authentication

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express')
 
 const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
+
 const connectDB = require('./config/db');
 //Load env vars
 
 dotenv.config({path:'./config/config.env'});
 connectDB();
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 const app=express();
 
 app.use(express.json());
+app.use (cookieParser());
 app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
+
 
 const PORT=process.env.PORT || 5000;
 const server = app.listen (PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ',PORT));
