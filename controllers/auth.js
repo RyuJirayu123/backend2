5cc1da8ec93fb2bff4056ab07797c539642a1a91
RyuJirayu123 <rew254967@gmail.com>
2025-03-24 14:27:41 +0700

    ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,10 +93,13 @@ const sendTokenResponse=(user,statusCode,res)=>{
     if(process.env.NODE_ENV==='production'){
         options.secure=true;
     }
-    res.status(statusCode).cookie('token',token,options).json({
-        success:true,
-        token
-    })
+    res.status(statusCode).cookie("token", token, options).json({
+        success: true,
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+      })
 }
 
 
